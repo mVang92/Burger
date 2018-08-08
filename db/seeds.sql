914880849c3de73359c50fb970f761426fcf3e11
mVang <meng.vang92@yahoo.com>
2018-08-08 16:07:42 -0500

    Initial Commit

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("California Burger", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Texas Triple Wooper", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Angry Burger", true);
