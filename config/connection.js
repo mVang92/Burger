0e84650b021982a2fc6f7573a0f6a4b70eb3826d
mVang <meng.vang92@yahoo.com>
2018-08-13 18:00:49 -0500

    connection.js updated with heroku connection

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,5 @@
 var mysql = require("mysql");
-
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "root",
-  database: "burgers_db"
-});
+var connection;
 
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL)
@@ -19,12 +12,6 @@ if (process.env.JAWSDB_URL) {
   });
 };
 
-connection.connect(function (err) {
-  if (err) {
-    console.error("Error connecting: " + err.stack);
-    return;
-  }
-  console.log("Connected as id " + connection.threadId);
-});
+connection.connect();
 
 module.exports = connection;
